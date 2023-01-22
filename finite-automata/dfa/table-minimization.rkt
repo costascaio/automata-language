6551f8c9bca82ab6e412575f87bafa00c3b7076e
costascaio <caio.soares@aluno.ufop.edu.br>
2023-01-21 21:19:08 -0300

    feat: add automata definitions on racket

---
+++ finite-automata/dfa/table-minimization.rkt
@@ -0,0 +1,137 @@
+#lang racket
+
+;; implementation of the DFA minimization
+;; algorithm using a fixpoint computation
+
+(provide refinement-table
+         minimize)
+
+(require "../fa.rkt"
+         "core.rkt"
+         "../../utils/set-extras.rkt")
+
+;; generating the initial partition
+
+(define (init-partition m)
+  (define final (dfa-final m))
+  (define states (dfa-states m))
+  (define (memfinal v)
+    (not (member v final)))
+  (list (filter memfinal states) final))
+
+;; checking if two states are equivalent
+
+(define (equiv? part m s1 s2)
+  ;; alphabet from DFA
+  (define sig (dfa-sigma m))
+  ;; transition function
+  (define delta (dfa-delta m))
+  ;; generating all possible triples
+  ;; from the states and alphabet symbols
+  (define to-test
+    (map (lambda (s)
+           (cons s1 (cons s2 s)))
+         sig))
+  (define (find-set s)
+    (findf (lambda (x)
+             (member s x))
+           part))
+  ;; checking if some triple is formed
+  ;; by equivalent states
+  (define (check-equiv t)
+    (match t
+      [(cons s1 (cons s2 s))
+       (let ([x1 (dict-ref delta
+                           (cons s1 s))]
+             [x2 (dict-ref delta
+                           (cons s2 s))])
+         (eq? (find-set x1)
+              (find-set x2)))]))
+  ;; checking all triples formed by the
+  ;; states and alphabet symbols.
+  (andmap check-equiv to-test))
+
+;; generating the next partition
+;; refinement
+
+(define (next-partition m current)
+  (append-map
+   (lambda (p)
+     (match p
+       [(cons s1 s2)
+        (group-by
+         (lambda (x) (equiv? current m s1 x))
+         p)]))
+   current))
+
+;; stop condition
+
+(define (stop? table)
+  (set=? (car table)
+         (cadr table)))
+
+;; create the sucessive refinement list
+
+(define (refine m current)
+  (let* ([next (next-partition
+                   m
+                   (car current))]
+         [tbl (cons next current)])
+    (if (stop? tbl)
+        tbl
+        (refine m tbl))))
+
+(define (refinement-table m)
+  (define start (init-partition m))
+  (refine m (list start)))
+
+;; constructing the minimal DFA
+
+(define (minimize m)
+  ;; getting the last partition
+  (define sts
+    (car (refinement-table m)))
+  ;; generating fresh names for states
+  (define (fresh n)
+    (string->symbol (format "s~a" n)))
+  (define fresh-names
+    (map (lambda (s n)
+           (cons s (fresh n)))
+         sts
+         (build-list (length sts)
+                     values)))
+  ;; applying the renaming
+  (define (rename s)
+    (findf (lambda (p) (member s (car p)))
+           fresh-names))
+  ;; start state
+  (define start
+    (cdr (rename (dfa-start m))))
+  ;; final states
+  (define finals
+    (map (lambda (s) (cdr (rename s)))
+     (flatten
+       (filter
+        (lambda (s)
+          (not (set-disjoint
+                s
+                (dfa-final m))))
+        sts))))
+  ;; building the transition function
+  (define (add-trans p)
+    (match p
+      [(cons (cons s1 c) s2)
+       (cons (cons (cdr (rename s1))
+                   c)
+             (cdr (rename s2)))]))
+  (define delta
+    (remove-duplicates
+     (map add-trans
+          (dfa-delta m))))
+  ;; finally, building the minimal
+  ;; dfa from its components
+  (mk-dfa (map cdr fresh-names)
+          (dfa-sigma m)
+          delta
+          start
+          finals))
---
+++ finite-automata/dfa/tests.rkt
@@ -0,0 +1,47 @@
+#lang racket
+
+
+(require "core.rkt"
+         "image-builder.rkt"
+         "table-minimization.rkt"
+         "../../utils/dot.rkt")
+
+
+;; simple example
+
+(define M
+  (dfa s1 [s3] (s1 : 0 -> s2)
+               (s2 : 1 -> s2)
+               (s2 : 0 -> s3)
+               (s3 : 0 -> s3)
+               (s3 : 1 -> s2)))
+
+
+
+(define even01
+  (dfa pp
+       (pp pi)
+       (pp : 0 -> ip)
+       (pp : 1 -> pi)
+       (ip : 0 -> pp)
+       (ip : 1 -> ii)
+       (pi : 0 -> ii)
+       (pi : 1 -> pp)
+       (ii : 0 -> pi)
+       (ii : 1 -> ip)))
+
+(define mod6
+  (dfa st0
+       (st0)
+       (st0 : 0 -> st0)
+       (st0 : 1 -> st1)
+       (st1 : 0 -> st2)
+       (st1 : 1 -> st3)
+       (st2 : 0 -> st4)
+       (st2 : 1 -> st5)
+       (st3 : 0 -> st0)
+       (st3 : 1 -> st1)
+       (st4 : 0 -> st2)
+       (st4 : 1 -> st3)
+       (st5 : 0 -> st4)
+       (st5 : 1 -> st5)))
