7fd0960bee3f5aee0f014e8c2c1b2678c1c4c43b
costascaio <caio.soares@aluno.ufop.edu.br>
2023-01-29 02:50:28 -0300

    feat: add intersection and union of two dfas

--- finite-automata/dfa/dfa-operations.rkt
+++ finite-automata/dfa/dfa-operations.rkt
@@ -9,30 +9,23 @@
 
 (define M
   (dfa A (B) (A : 0 -> B)
-             (B : 1 -> B)
-             (B : 0 -> B)))
+       (B : 1 -> B)
+       (B : 0 -> B)))
 
 
 (define N
   (dfa C (D) (C : 0 -> C)
-             (C : 1 -> D)
-             (D : 0 -> C)
-             (D : 1 -> D)))
+       (C : 1 -> D)
+       (D : 0 -> C)
+       (D : 1 -> D)))
 
-(dfa->pict N)
-
-; append list of the states
-(define (product-states dfa1 dfa2)
-  (append (dfa-states dfa1) (dfa-states dfa2)))
+;(dfa->pict N)
 
 ; generate a list with the initial states of dfa
 (define (product-start dfa1 dfa2)
-  (list (dfa-start dfa1) (dfa-start dfa2)))
-
-
-;(define (dfa-product dfa1 dfa2)
-;  (define D (pair-initial dfa1 dfa2)))
-
+  (convert-string-symbol (combination-states
+                          (list (dfa-start dfa1))
+                          (list (dfa-start dfa2)))))
 
 ; convert a list of symbols to string
 (define (convert-symbol-string list-symbols)
@@ -47,29 +40,25 @@
   (define str-states1 (convert-symbol-string states1))
   (define str-states2 (convert-symbol-string states2))
   (apply append (map (lambda (s1) 
-    (map (lambda (s2)
-      (string-append s1 s2)) str-states2)) str-states1)))
+                       (map (lambda (s2)
+                              (string-append s1 s2)) str-states2)) str-states1)))
 
 ; generate a list with the combination of states from two dfa
-(define (list-states dfa1 dfa2)
+(define (product-states dfa1 dfa2)
   (define states-combination 
     (combination-states 
-      (dfa-states dfa1) (dfa-states dfa2)))
+     (dfa-states dfa1) (dfa-states dfa2)))
   (convert-string-symbol states-combination))
-
 
 ; generate a list of all transitions that starts with a state
 (define (find-state-transitions state list-delta)
-    (filter (lambda (transition)
-           (eq? state (car (car transition)))) list-delta))
+  (filter (lambda (transition)
+            (eq? state (car (car transition)))) list-delta))
 
 ; generate a list of all transitions that has a espected symbol
 (define (find-symbol-transitions symb list-delta)
-    (filter (lambda (transition)
-           (eq? symb (cdr (car transition)))) list-delta))
-
-(find-state-transitions (dfa-start M) (dfa-delta M))
-(find-state-transitions (dfa-start N) (dfa-delta N))
+  (filter (lambda (transition)
+            (eq? symb (cdr (car transition)))) list-delta))
 
 ; generate a product of n transitions from different fa considering two differnt states
 (define (product-transitions list-delta1 list-delta2 state1 state2 symb)
@@ -77,20 +66,61 @@
   (define s2 (find-state-transitions state2 list-delta2))
   (define t1 (find-symbol-transitions symb s1))
   (define t2 (find-symbol-transitions symb s2))
-  (apply append (map (lambda (s1)
-    (map (lambda (s2)
-      (product-transition s1 s2 symb)) t2)) t1)))
+  (first (apply append (map (lambda (s1)
+                              (map (lambda (s2)
+                                     (product-transition s1 s2 symb)) t2)) t1))))
 
 ; generate a product of two transitions of a symbol
 (define (product-transition transition1 transition2 symb)
   (define origin-state 
     (string->symbol (string-append 
-      (symbol->string (car (car transition1)))
-      (symbol->string (car (car transition2))))))
+                     (symbol->string (car (car transition1)))
+                     (symbol->string (car (car transition2))))))
   (define destiny-state 
     (string->symbol (string-append 
-      (symbol->string (cdr transition1))
-      (symbol->string (cdr transition2)))))
+                     (symbol->string (cdr transition1))
+                     (symbol->string (cdr transition2)))))
   (cons (cons origin-state symb) destiny-state))
 
-(product-transitions (dfa-delta M) (dfa-delta N) (dfa-start M) (dfa-start N) (first (dfa-sigma M)))
+
+; generate a list with a cartesian product of all transitions of two dfa
+(define (dfa-product dfa1 dfa2)
+  (apply append (map (lambda (s1)
+                       (apply append (map (lambda (s2)
+                                            (map (lambda (symb)
+                                                   (product-transitions (dfa-delta dfa1) (dfa-delta dfa2) s1 s2 symb))
+                                                 (dfa-sigma dfa1)))
+                                          (dfa-states dfa2))))
+                     (dfa-states dfa1))))
+
+;(dfa-product M N)
+;(dfa-final M)
+
+(product-states M N)
+
+; function that returns if a string is a substring of another string
+(define (substring? substring string)
+  (regexp-match (regexp substring) string))
+
+; generate a list with all final states considering a intersection of two dfas
+(define (dfa-intersection-states dfa1 dfa2)
+  (convert-string-symbol (combination-states (dfa-final dfa1)
+                                             (dfa-final dfa2))))
+
+; generate a list with all final states considering a union of two dfas
+(define (find-union-states list-states final-state)
+  (define final (symbol->string final-state))
+  (filter (lambda (state)
+            (substring? final (symbol->string state)))
+          list-states))
+
+(define (dfa-union-states dfa1 dfa2)
+  (define list-states (product-states dfa1 dfa2))
+  (define final-states (append (dfa-final dfa1) (dfa-final dfa2)))
+  (remove-duplicates (flatten
+                      (map (lambda (state)
+                             (find-union-states list-states state)) final-states))))
+
+(dfa-union-states M N)
+;(define (mk-intersection-dfa dfa1 dfa2)
+;  )
