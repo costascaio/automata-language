dbe6a840fccc78eb76249eac42bd5e66d1d2e754
costascaio <caio.soares@aluno.ufop.edu.br>
2023-05-23 16:30:42 -0300

    fix: add functions to make a list of transitions of a nfa

--- finite-automata/dfa/tests.rkt
+++ finite-automata/dfa/tests.rkt
@@ -17,6 +17,41 @@
        (D : 0 -> B)
        (D : 1 -> C)))
 
-(dfa->pict T)
-(dfa->pict (minimize T))
-(dfa->pict (minimization T))
+(define A
+  (dfa A (B)
+       (A : 0 -> B)
+       (A : 1 -> B)
+       (B : 0 -> B)
+       (B : 1 -> B)))
+
+(define reverse (revert-delta (dfa-delta A)))
+
+(revert-delta (dfa-delta A))
+
+; function that group all the transitions that has the same beginning state and same symbol
+(define (test delta)
+  (remove-duplicates
+   (map (lambda (t1)
+         (filter (lambda (t2)
+                (if (and (eqv? (car (car t1)) (car (car t2)))
+                     (eqv? (cdr (car t1)) (cdr (car t2))))
+                    (cons (cons (car (car t2)) (cdr (car t2))) (cdr t2))
+                    #f
+                    )) delta)) delta )))
+
+(test reverse)
+; generate a list of final states of each grouped transition 
+(define (solution grouped-transitions)
+  (map (lambda (transitions)
+         (map (lambda (single-transition)
+                (cdr single-transition)) transitions)) grouped-transitions))
+
+(define (concat-grouped-transitions grouped-transitions destiny-states)
+  (map (lambda (group)
+         (cons (car (first group)) (list (list-ref destiny-states (index-of grouped-transitions group))))) grouped-transitions))
+
+(define s (solution (test reverse)))
+(concat-grouped-transitions (test reverse) (solution (test reverse)) )
+;(dfa->pict T)
+;(dfa->pict (minimize T))
+;(dfa->pict (minimization T))
